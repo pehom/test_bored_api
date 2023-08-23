520853aadee7b971ff47a7aca08b7a17c8124719
Supreme Commander <pehom@mail.ru>
2023-08-23 23:26:57 +0300

    refactored name for test_get_random_activity_by_key

--- tests/test_find_activity_by_key/test_find_activity_by_key.py
+++ tests/test_find_activity_by_key/test_find_activity_by_key.py
@@ -7,8 +7,8 @@ import pytest
     'hui',
     8125168
 ])
-def test_get_random_activity_by_existing_key(key, get_random_activity_by_key):
-    get_random_activity_by_key.assert_status_code([404]).valid()
+def test_get_random_activity_by_key(key, get_random_activity_by_key):
+    get_random_activity_by_key.assert_status_code([200, 404]).valid()
 
 
 
